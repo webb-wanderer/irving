d5287936899070c28e8c5058571bc92b7d2b5dbb
webb-wanderer <140278726+webb-wanderer@users.noreply.github.com>
2024-12-01 16:36:57 -0800

    add navbar

---
+++ _assets/js/footer-irving-min.js
@@ -0,0 +1,1 @@
+$("#nav-hhp").load("/_assets/includes/nav-hhp.htm"),$("#nav-di").load("/toc.htm");
---
+++ _assets/js/footer-irving.js
@@ -0,0 +1,2 @@
+$( "#nav-hhp" ).load( "/_assets/includes/nav-hhp.htm" );
+$( "#nav-di" ).load( "/toc.htm" );
